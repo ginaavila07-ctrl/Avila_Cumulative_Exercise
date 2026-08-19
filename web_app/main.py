6384c20a24ad7acf9f506b786c0787c7e02f02f8
unknown <gina.avila07@gmai.com>
2026-08-19 12:27:42 -0500

    add latitude and Longitude to code

--- web_app/main.py
+++ web_app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+import requests
 
 VALID_STATES = {
     "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE",
@@ -15,10 +16,36 @@ avila_app = FastAPI()
 def home():
     return{"message": "Hello, World Working on my first API Fast"}
 
-import requests
+def get_coordinates(city:str, state:str):
+      url= "https://geocoding-api.open-meteo.com/v1/search"
+
+      params = {
+            "name":f"{city},{state}",
+            "count": 1,
+            "language": "en",
+            "format" : "json",
+      }
+      response = requests.get(url, params=params, timeout=5)
+      response.raise_for_status()
+
+      data = response.json()
+
+      if not data.get("results"):
+            return None
+
+      location = data["results"][0]
+
+    
+      return {
+            "latitude": location["latitude"],
+            "longitude": location["longitude"],
+      }
+
 
 @avila_app.post("/geocode/{city}/{state}")
 def geocode(city: str, state:str):
+
+        city = city.strip()
 
         if not city.strip():
               raise HTTPException(
@@ -27,24 +54,35 @@ def geocode(city: str, state:str):
               )
         
 
-        if not state.strip():
+        state = state.strip().upper()
+
+        if not state:
               raise HTTPException(
                     status_code=400,
-                    detail="Stats is required"
+                    detail="State is required"
               )
 
-        state = state.strip().upper()
+        
 
         if state not in VALID_STATES:
               raise HTTPException(
                     status_code=400,
-                    details="Invaild state. Use a two-letter U.S. state abbreviation"
+                    detail="Invalid state. Use a two-letter U.S. state abbreviation"
               )
-        return{
-        "city": city,
-        "state": state
+        coordinates = get_coordinates(city, state)
 
-    }
+        if coordinates is None:
+              raise HTTPException(
+                    status_code=404,
+                    detail="Location not found"
+              )
+        
+        return{
+            "city": city,
+            "state": state,
+            "latitude": coordinates["latitude"],
+            "longitude": coordinates["longitude"],  
+        }
 
 
 
