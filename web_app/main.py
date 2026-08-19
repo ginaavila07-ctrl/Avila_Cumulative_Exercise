92e8713455d13201d06e3c7e02fd5cc2b947d8ff
unknown <gina.avila07@gmai.com>
2026-08-19 10:43:34 -0500

    update states to state

--- web_app/main.py
+++ web_app/main.py
@@ -4,3 +4,13 @@ avila_app = FastAPI()
 @avila_app.get("/")
 def home():
     return{"message": "Hello, World Working on my first API Fast"}
+
+import requests
+
+@avila_app.post("/geocode/{city}/{state}")
+def geocode(city: str, state:str):
+    return{
+        "city": city,
+        "state": state
+    }
+
